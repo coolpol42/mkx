917602b2a5a1b61c3dc00a6806d846157bea7800
Josef Polák <coolpol42@gmail.com>
2023-05-19 17:25:48 +0200

    Empty dialog

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -4,12 +4,30 @@ sap.ui.define([
     "sap/ui/model/json/JSONModel",
     "sap/ui/model/resource/ResourceModel",
     "sap/ui/model/odata/v2/ODataModel",
-], function (Controller, MessageToast, JSONModel, ODataModel) {
+    "sap/ui/core/Fragment"
+], function (Controller, MessageToast, JSONModel, ODataModel, Fragment) {
     "use strict";
     return Controller.extend("sap.ui.mkx.controller.App", {
         changeLanguage: function (oEvent) {
             var selectedKey = oEvent.getSource().getSelectedKey();
             sap.ui.getCore().getConfiguration().setLanguage(selectedKey);
+        },
+        saveValues: function (){
+            let data = this.getView().getModel("data").getProperty("record");
+        },
+        openHistory: function (){
+            var oView = this.getView();
+
+            if (!this.byId("historyDialog")) {
+                var oDialog = sap.ui.xmlfragment(oView.getId(), "sap.ui.mkx.view.History", this);
+                oView.addDependent(oDialog);
+                oDialog.open();
+            } else {
+                this.byId("historyDialog").open();
+            }
+        },
+        closeHistory: function (){
+            this.byId("historyDialog").close()
         }
     });
 })
